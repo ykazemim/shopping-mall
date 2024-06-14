32434b72f57796a5e4d1a64facbe61b2eabf8c32
mk master <79350888+MKmasterg@users.noreply.github.com>
2024-06-14 13:34:47 +0330

    Fix a bug in sql statemnet when inserting into client table

--- sql_handling/src/User.java
+++ sql_handling/src/User.java
@@ -1,34 +1,46 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class User {
     private Session newSession = null;
 
     public User(Connection connection, String name, String username, String password, String phone, String address) throws Exception {
         // Since only clients allowed to sign up in this app, this constructor only create client instances
+
         String hashedPassword = PasswordHasher.hashPassword(password);
         try{
-            String st = """
-                    -- Insert into User table
-                    INSERT INTO User (username, password, name, phone, user_type) values (?, ?, ?, ?, ?);
-                    -- Retrieve the last inserted user ID
-                    SET @last_user_id = LAST_INSERT_ID();
-                    -- Insert into Client table using the retrieved user ID
-                    INSERT INTO Client (address, credit, user) values (?, ?, @last_user_id);""";
-
-            PreparedStatement preparedStatement = connection.prepareStatement(st);
+            // Insert into User table
+            String userStatement = "INSERT INTO User (username, password, name, phone, user_type) values (?, ?, ?, ?, ?);";
+            PreparedStatement preparedStatement = connection.prepareStatement(userStatement, Statement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1,username);
             preparedStatement.setString(2,hashedPassword);
             preparedStatement.setString(3,name);
             preparedStatement.setString(4,phone);
             preparedStatement.setString(5,"CLIENT");
-            preparedStatement.setString(6,address);
-            preparedStatement.setFloat(7,0f);
+            preparedStatement.executeUpdate();
 
-            preparedStatement.execute();
+            // Retrieve the last inserted user ID
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            int idUser = -1;
+            if(generatedKeys.next()){
+                idUser = generatedKeys.getInt(1);
+            }
 
+            if(idUser == -1)
+                throw new Exception("Something went wrong in getting user id");
+
+            // Insert into Client table using the retrieved user ID
+            String clientStatement = "INSERT INTO Client (address, credit, user) values (?, ?, ?);";
+            preparedStatement = connection.prepareStatement(clientStatement);
+
+            preparedStatement.setString(1,address);
+            preparedStatement.setFloat(2,0f);
+            preparedStatement.setInt(3, idUser);
+
+            preparedStatement.executeUpdate();
+
+            // Set the session
             newSession = new Session(connection, username, password);
+
         } catch (SQLException e){
             System.out.println(e.getMessage());
         }
