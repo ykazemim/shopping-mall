ad131ee01b3528c1c8e4dc736d9a4c4750ab768e
mk master <79350888+MKmasterg@users.noreply.github.com>
2024-06-14 12:14:05 +0330

    Change return type of sign in validator

--- sql_handling/src/Validator.java
+++ sql_handling/src/Validator.java
@@ -1,19 +1,21 @@
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
     private final static Pattern passwordPattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
 
-    public static boolean validateSignInForm(String username, String password) throws Exception {
+    public static ArrayList<String> validateSignInForm(String username, String password) {
+        ArrayList<String> errors = new ArrayList<>();
         if (username.length() < 4){
-            throw new Exception("Short username");
+            errors.add("Short username");
         }
 
         Matcher passwordMatcher = passwordPattern.matcher(password);
-        if(passwordMatcher.matches())
-            return true;
-        else
-            throw new Exception("Invalid password");
+        if(!passwordMatcher.matches())
+            errors.add("Invalid password");
+
+        return errors;
     }
 
 }
