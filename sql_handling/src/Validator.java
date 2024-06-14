e92fa4fa3bf9d8468813107e38a3a173847c0b22
mk master <79350888+MKmasterg@users.noreply.github.com>
2024-06-14 12:35:12 +0330

    Add username,name and phone patterns to check validaty of sign up form in validator class

--- sql_handling/src/Validator.java
+++ sql_handling/src/Validator.java
@@ -4,6 +4,9 @@ import java.util.regex.Pattern;
 
 public class Validator {
     private final static Pattern passwordPattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+    private final static Pattern usernamePattern = Pattern.compile("^[A-Za-z0-9_-]+$");
+    private final static Pattern namePattern = Pattern.compile("^[A-Za-z\\s]+$");
+    private final static Pattern phonePattern = Pattern.compile("^09\\d*$");
 
     public static ArrayList<String> validateSignInForm(String username, String password) {
         ArrayList<String> errors = new ArrayList<>();
@@ -11,9 +14,44 @@ public class Validator {
             errors.add("Short username");
         }
 
+        Matcher usernameMatcher = usernamePattern.matcher(username);
+        if (!usernameMatcher.matches())
+            errors.add("Username should only contains alphabetic characters, digits, underscores and dashes");
+
         Matcher passwordMatcher = passwordPattern.matcher(password);
         if(!passwordMatcher.matches())
             errors.add("Invalid password");
+
+        return errors;
+    }
+
+    public static ArrayList<String> validateSingUpForm(String name, String username, String password, String phone, String address) {
+        ArrayList<String> errors = new ArrayList<>();
+
+        Matcher nameMatcher = namePattern.matcher(name);
+        if (!nameMatcher.matches())
+            errors.add("Invalid name");
+
+        if (username.length() < 4){
+            errors.add("Short username");
+        }
+
+        Matcher usernameMatcher = usernamePattern.matcher(username);
+        if (!usernameMatcher.matches())
+            errors.add("Username should only contains alphabetic characters, digits, underscores and dashes");
+
+        Matcher passwordMatcher = passwordPattern.matcher(password);
+        if(!passwordMatcher.matches())
+            errors.add("Invalid password");
+
+        Matcher phoneMatcher = phonePattern.matcher(phone);
+        if (phone.length() != 11 || !phoneMatcher.matches()) {
+            errors.add("Please enter phone number in this pattern : 09xxxxxxxxx");
+        }
+
+        if (address.isBlank()) {
+            errors.add("Invalid address");
+        }
 
         return errors;
     }
