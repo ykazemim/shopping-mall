249ec1cce8f7c5b8d9e9e3d44b8e268a9188d71a
mk master <79350888+MKmasterg@users.noreply.github.com>
2024-06-15 11:09:12 +0330

    Add product class in data type classes package and add delete,modify and fetch in poduct handler class

---
+++ panels/src/DataTypeClasses/Product.java
@@ -0,0 +1,87 @@
+package DataTypeClasses;
+
+public class Product {
+    private int idProduct;
+    private String title;
+    private float price;
+    private String pathToImage;
+    private int stock;
+    private int ratingCount;
+    private float averageRating;
+    private boolean availableForClient;
+
+    public Product(int idProduct, String title, float price, String pathToImage, int stock, int ratingCount, float averageRating, boolean availableForClient) {
+        this.idProduct = idProduct;
+        this.title = title;
+        this.price = price;
+        this.pathToImage = pathToImage;
+        this.stock = stock;
+        this.ratingCount = ratingCount;
+        this.averageRating = averageRating;
+        this.availableForClient = availableForClient;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getPathToImage() {
+        return pathToImage;
+    }
+
+    public void setPathToImage(String pathToImage) {
+        this.pathToImage = pathToImage;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getRatingCount() {
+        return ratingCount;
+    }
+
+    public void setRatingCount(int ratingCount) {
+        this.ratingCount = ratingCount;
+    }
+
+    public float getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(float averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public boolean isAvailableForClient() {
+        return availableForClient;
+    }
+
+    public void setAvailableForClient(boolean availableForClient) {
+        this.availableForClient = availableForClient;
+    }
+}
--- panels/src/SQLDefinedClasses/ProductHandler.java
+++ panels/src/SQLDefinedClasses/ProductHandler.java
@@ -1,5 +1,7 @@
 package SQLDefinedClasses;
 
+import DataTypeClasses.Product;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -22,6 +24,54 @@ public class ProductHandler {
             System.out.println("Something went wrong in adding product to the database");
             System.out.println(e.getMessage());
         }
-
     }
+
+    public static void updateProduct(Connection connection, int id, String title, float price, boolean availableForClient, String pathToImage, int stock) {
+        // Update product in the database
+        try {
+            String productStatement = "UPDATE product SET title = ?, price = ?, image = ?, stock = ?, available_for_client = ? WHERE id = ?;";
+            java.sql.PreparedStatement preparedStatement = connection.prepareStatement(productStatement);
+            preparedStatement.setString(1, title);
+            preparedStatement.setFloat(2, price);
+            preparedStatement.setString(3, pathToImage);
+            preparedStatement.setInt(4, stock);
+            preparedStatement.setBoolean(5, availableForClient);
+            preparedStatement.setInt(6, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e){
+            System.out.println("Something went wrong in updating product in the database");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void deleteProduct(Connection connection, int id) {
+        // Delete product from the database
+        try {
+            String productStatement = "DELETE FROM product WHERE id = ?;";
+            java.sql.PreparedStatement preparedStatement = connection.prepareStatement(productStatement);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e){
+            System.out.println("Something went wrong in deleting product from the database");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static Product fetchProduct(Connection connection, int id) {
+        // Fetch product from the database
+        try {
+            String productStatement = "SELECT * FROM product WHERE id = ?;";
+            java.sql.PreparedStatement preparedStatement = connection.prepareStatement(productStatement);
+            preparedStatement.setInt(1, id);
+            java.sql.ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new Product(resultSet.getInt("id"), resultSet.getString("title"), resultSet.getFloat("price"), resultSet.getString("image"), resultSet.getInt("stock"), resultSet.getInt("rating_count"), resultSet.getFloat("average_rating"), resultSet.getBoolean("available_for_client"));
+            }
+        } catch (SQLException e){
+            System.out.println("Something went wrong in fetching product from the database");
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
 }
