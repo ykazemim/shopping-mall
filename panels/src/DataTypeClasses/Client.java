734835a2ed3ba6dc1d88f0360eefc935fef88c98
mk master <79350888+MKmasterg@users.noreply.github.com>
2024-06-16 20:06:23 +0330

    Add name,phone and username to Client class

--- panels/src/DataTypeClasses/Client.java
+++ panels/src/DataTypeClasses/Client.java
@@ -5,15 +5,50 @@ public class Client {
     private final int iduser;
     private String clientAddress;
     private float clientCredit;
-    public Client(int idclient, int iduser ,String clientAddress, float clientCredit) {
+    private String clientName;
+    private String clientPhone;
+    private String username;
+
+    public Client(int idclient, int iduser ,String clientAddress, float clientCredit, String clientName, String clientPhone, String username) {
         this.idclient = idclient;
         this.iduser = iduser;
         this.clientAddress = clientAddress;
         this.clientCredit = clientCredit;
+        this.clientName = clientName;
+        this.clientPhone = clientPhone;
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientPhone() {
+        return clientPhone;
+    }
+
+    public void setClientPhone(String clientPhone) {
+        this.clientPhone = clientPhone;
     }
 
     public int getIdclient() {
         return idclient;
+    }
+
+    public int getIduser() {
+        return iduser;
     }
 
     public String getClientAddress() {
