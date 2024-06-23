c8b6818d0fc2f9fac9ecd2ddfe17c855cda6171b
mk master <79350888+MKmasterg@users.noreply.github.com>
2024-06-24 00:25:10 +0330

    Add admin creation in InitializeDB

---
+++ initialization/src/Validator.java
@@ -0,0 +1,37 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.File;
+
+public class Validator {
+    private final static Pattern passwordPattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+    private final static Pattern usernamePattern = Pattern.compile("^[A-Za-z0-9_-]+$");
+    private final static Pattern namePattern = Pattern.compile("^[A-Za-z\\s]+$");
+    private final static Pattern phonePattern = Pattern.compile("^09\\d*$");
+
+    public static ArrayList<String> validateSingUpForm(String name, String username, String password, String phone) {
+        ArrayList<String> errors = new ArrayList<>();
+
+        Matcher nameMatcher = namePattern.matcher(name);
+        if (!nameMatcher.matches())
+            errors.add("Name should only contain alphabetic characters and white spaces");
+
+        Matcher usernameMatcher = usernamePattern.matcher(username);
+        if (!usernameMatcher.matches() || username.length() < 4)
+            errors.add("Username should at least contain 4 characters and only contain alphabetic characters, digits, underscores and dashes");
+
+        Matcher passwordMatcher = passwordPattern.matcher(password);
+        if(!passwordMatcher.matches())
+            errors.add("Password's length should be at least 8 and it should contains at least one uppercase letter, one lowercase letter, one digit and one special characters");
+
+        Matcher phoneMatcher = phonePattern.matcher(phone);
+        if (phone.length() != 11 || !phoneMatcher.matches()) {
+            errors.add("Please enter phone number in this pattern : 09xxxxxxxxx");
+        }
+
+        return errors;
+    }
+
+}
