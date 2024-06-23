93ccd31b0e6ca79093981e257054bd2ac61dc560
Younes Kazemi M. <ykazemim04@gmail.com>
2024-06-24 02:08:40 +0330

    Set an icon for the app

--- panels/src/Main.java
+++ panels/src/Main.java
@@ -1,5 +1,14 @@
+/**
+ * This app's icon is downloaded from:
+ * https://www.flaticon.com/free-icon/shopping-bag_743007?term=shopping+mall&page=1&position=26&origin=tag&related_id=743007
+ */
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
     private static final String username = "root";
@@ -7,14 +16,23 @@ public class Main {
     private static final String dbURL = "jdbc:mysql://localhost:3306/mall";
     private static JFrame frame;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         // Create a connection to sql server
         new Initialize(dbURL, username, password);
 
         frame = new JFrame("Shopping Mall");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(1,1));
+        frame.setLayout(new GridLayout(1, 1));
+
+        // Setting the app's icon
+        try {
+            BufferedImage img = ImageIO.read(Objects.requireNonNull(Main.class.getResource("app_icon.png")));
+            frame.setIconImage(img);
+        } catch (IllegalArgumentException | IOException e) {
+            System.out.println("Failed to set the app's icon.");
+        }
+
 //        frame.setResizable(false);
 
 
@@ -24,7 +42,7 @@ public class Main {
 //        AddProductPanel addProductPanel = new aAddProductPanel();
 //        frame.add(addProductPanel);
 
-        Initialize.setSession(new Session(Initialize.connection,"amir_125","Moli@1299"));
+        Initialize.setSession(new Session(Initialize.connection, "amir_125", "Moli@1299"));
         AdminProductsPanel adminProductsPanel = new AdminProductsPanel();
         frame.add(adminProductsPanel);
 
