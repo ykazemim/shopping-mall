7ce4d000ea2971ac0f9c644f9a19f0db7aba7d42
mk master <79350888+MKmasterg@users.noreply.github.com>
2024-06-25 01:00:18 +0330

    Add Validator for edit admin profile

--- panels/src/Validator.java
+++ panels/src/Validator.java
@@ -91,5 +91,27 @@ public class Validator {
         return errors;
     }
 
+    public static ArrayList<String> validateEditAdminProfileForm(String name, String username, String password, String phone){
+        ArrayList<String> errors = new ArrayList<>();
+
+        Matcher nameMatcher = namePattern.matcher(name);
+        if (!nameMatcher.matches())
+            errors.add("Name should only contain alphabetic characters and white spaces");
+
+        Matcher usernameMatcher = usernamePattern.matcher(username);
+        if (!usernameMatcher.matches() || username.length() < 4)
+            errors.add("Username should at least contain 4 characters and only contain alphabetic characters, digits, underscores and dashes");
+
+        Matcher passwordMatcher = passwordPattern.matcher(password);
+        if(!passwordMatcher.matches())
+            errors.add("Password's length should be at least 8 and it should contains at least one uppercase letter, one lowercase letter, one digit and one special characters");
+
+        Matcher phoneMatcher = phonePattern.matcher(phone);
+        if (phone.length() != 11 || !phoneMatcher.matches()) {
+            errors.add("Please enter phone number in this pattern : 09xxxxxxxxx");
+        }
+
+        return errors;
+    }
 
 }
